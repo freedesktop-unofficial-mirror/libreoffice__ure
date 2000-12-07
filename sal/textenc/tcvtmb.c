7fe7a98115cc54c51fee515234c5a48f72bf3e1b
th <th@openoffice.org>
2000-12-07 09:48:42 +0000

    #81346# - Fix for unconverted characters for DBCS encodings

--- sal/textenc/tcvtmb.c
+++ sal/textenc/tcvtmb.c
@@ -1,60 +1,60 @@
 /*************************************************************************
  *
- *  $RCSfile: tcvtmb.c,v $
+ *	$RCSfile: tcvtmb.c,v $
  *
- *  $Revision: 1.1.1.1 $
+ *	$Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 15:17:29 $
+ *	last change: $Author: th $ $Date: 2000-12-07 10:48:42 $
  *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
+ *	The Contents of this file are made available subject to the terms of
+ *	either of the following licenses
  *
- *         - GNU Lesser General Public License Version 2.1
- *         - Sun Industry Standards Source License Version 1.1
+ *		   - GNU Lesser General Public License Version 2.1
+ *		   - Sun Industry Standards Source License Version 1.1
  *
- *  Sun Microsystems Inc., October, 2000
+ *	Sun Microsystems Inc., October, 2000
  *
- *  GNU Lesser General Public License Version 2.1
- *  =============================================
- *  Copyright 2000 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *	GNU Lesser General Public License Version 2.1
+ *	=============================================
+ *	Copyright 2000 by Sun Microsystems, Inc.
+ *	901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License version 2.1, as published by the Free Software Foundation.
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License version 2.1, as published by the Free Software Foundation.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *	MA	02111-1307	USA
  *
  *
- *  Sun Industry Standards Source License Version 1.1
- *  =================================================
- *  The contents of this file are subject to the Sun Industry Standards
- *  Source License Version 1.1 (the "License"); You may not use this file
- *  except in compliance with the License. You may obtain a copy of the
- *  License at http://www.openoffice.org/license.html.
+ *	Sun Industry Standards Source License Version 1.1
+ *	=================================================
+ *	The contents of this file are subject to the Sun Industry Standards
+ *	Source License Version 1.1 (the "License"); You may not use this file
+ *	except in compliance with the License. You may obtain a copy of the
+ *	License at http://www.openoffice.org/license.html.
  *
- *  Software provided under this License is provided on an "AS IS" basis,
- *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
- *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
- *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
- *  See the License for the specific provisions governing your rights and
- *  obligations concerning the Software.
+ *	Software provided under this License is provided on an "AS IS" basis,
+ *	WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *	WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *	MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *	See the License for the specific provisions governing your rights and
+ *	obligations concerning the Software.
  *
- *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *	The Initial Developer of the Original Code is: Sun Microsystems, Inc.
  *
- *  Copyright: 2000 by Sun Microsystems, Inc.
+ *	Copyright: 2000 by Sun Microsystems, Inc.
  *
- *  All Rights Reserved.
+ *	All Rights Reserved.
  *
- *  Contributor(s): _______________________________________
+ *	Contributor(s): _______________________________________
  *
  *
  ************************************************************************/
@@ -326,30 +326,30 @@ sal_Size ImplUnicodeToDBCS( const ImplTextConverterData* pData, void* pContext,
                 if ( nFlags & RTL_UNICODETOTEXT_FLAGS_PRIVATE_MAPTO0 )
                     cConv = (sal_Char)(sal_uChar)(c & 0xFF);
             }
+        }
 
-            if ( !cConv )
+        if ( !cConv )
+        {
+            if ( nFlags & RTL_UNICODETOTEXT_FLAGS_UNDEFINED_REPLACE )
             {
-                if ( nFlags & RTL_UNICODETOTEXT_FLAGS_UNDEFINED_REPLACE )
-                {
-                    /* !!! */
-                }
-
-                if ( nFlags & RTL_UNICODETOTEXT_FLAGS_UNDEFINED_REPLACESTR )
-                {
-                    /* !!! */
-                }
-
-                /* Handle undefined and surrogates characters */
-                /* (all surrogates characters are undefined) */
-                nAction = ImplHandleUndefinedUnicodeToTextChar( pData,
-                                                                &pSrcBuf, pEndSrcBuf,
-                                                                &pDestBuf, pEndDestBuf,
-                                                                nFlags, pInfo );
-                if ( nAction == IMPL_TEXTCVT_BREAK )
-                    break;
-                else
-                    continue;
+                /* !!! */
             }
+
+            if ( nFlags & RTL_UNICODETOTEXT_FLAGS_UNDEFINED_REPLACESTR )
+            {
+                /* !!! */
+            }
+
+            /* Handle undefined and surrogates characters */
+            /* (all surrogates characters are undefined) */
+            nAction = ImplHandleUndefinedUnicodeToTextChar( pData,
+                                                            &pSrcBuf, pEndSrcBuf,
+                                                            &pDestBuf, pEndDestBuf,
+                                                            nFlags, pInfo );
+            if ( nAction == IMPL_TEXTCVT_BREAK )
+                break;
+            else
+                continue;
         }
 
         /* SingleByte */
@@ -724,7 +724,7 @@ sal_Size ImplUnicodeToJISX0208( const ImplTextConverterData* pData,
         else
             cConv = 0;
 
-        if (cConv != 0)
+        if ( cConv != 0 )
         {
             if ( pDestBuf+1 >=	pEndDestBuf )
             {
