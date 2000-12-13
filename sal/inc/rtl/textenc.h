aaf64b1bb4b2888775fcd29fa211d7c0fa176882
th <th@openoffice.org>
2000-12-13 21:14:03 +0000

    #81658# - add MS_1361/JOHAB

--- sal/inc/rtl/textenc.h
+++ sal/inc/rtl/textenc.h
@@ -1,60 +1,60 @@
 /*************************************************************************
  *
- *  $RCSfile: textenc.h,v $
+ *	$RCSfile: textenc.h,v $
  *
- *  $Revision: 1.1.1.1 $
+ *	$Revision: 1.2 $
  *
- *  last change: $Author: hr $ $Date: 2000-09-18 15:17:15 $
+ *	last change: $Author: th $ $Date: 2000-12-13 22:14:03 $
  *
- *  The Contents of this file are made available subject to the terms of
- *  either of the following licenses
+ *	The Contents of this file are made available subject to the terms of
+ *	either of the following licenses
  *
- *         - GNU Lesser General Public License Version 2.1
- *         - Sun Industry Standards Source License Version 1.1
+ *		   - GNU Lesser General Public License Version 2.1
+ *		   - Sun Industry Standards Source License Version 1.1
  *
- *  Sun Microsystems Inc., October, 2000
+ *	Sun Microsystems Inc., October, 2000
  *
- *  GNU Lesser General Public License Version 2.1
- *  =============================================
- *  Copyright 2000 by Sun Microsystems, Inc.
- *  901 San Antonio Road, Palo Alto, CA 94303, USA
+ *	GNU Lesser General Public License Version 2.1
+ *	=============================================
+ *	Copyright 2000 by Sun Microsystems, Inc.
+ *	901 San Antonio Road, Palo Alto, CA 94303, USA
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public
- *  License version 2.1, as published by the Free Software Foundation.
+ *	This library is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ *	License version 2.1, as published by the Free Software Foundation.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *  Lesser General Public License for more details.
+ *	This library is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *	Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *  MA  02111-1307  USA
+ *	You should have received a copy of the GNU Lesser General Public
+ *	License along with this library; if not, write to the Free Software
+ *	Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *	MA	02111-1307	USA
  *
  *
- *  Sun Industry Standards Source License Version 1.1
- *  =================================================
- *  The contents of this file are subject to the Sun Industry Standards
- *  Source License Version 1.1 (the "License"); You may not use this file
- *  except in compliance with the License. You may obtain a copy of the
- *  License at http://www.openoffice.org/license.html.
+ *	Sun Industry Standards Source License Version 1.1
+ *	=================================================
+ *	The contents of this file are subject to the Sun Industry Standards
+ *	Source License Version 1.1 (the "License"); You may not use this file
+ *	except in compliance with the License. You may obtain a copy of the
+ *	License at http://www.openoffice.org/license.html.
  *
- *  Software provided under this License is provided on an "AS IS" basis,
- *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
- *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
- *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
- *  See the License for the specific provisions governing your rights and
- *  obligations concerning the Software.
+ *	Software provided under this License is provided on an "AS IS" basis,
+ *	WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *	WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *	MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *	See the License for the specific provisions governing your rights and
+ *	obligations concerning the Software.
  *
- *  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ *	The Initial Developer of the Original Code is: Sun Microsystems, Inc.
  *
- *  Copyright: 2000 by Sun Microsystems, Inc.
+ *	Copyright: 2000 by Sun Microsystems, Inc.
  *
- *  All Rights Reserved.
+ *	All Rights Reserved.
  *
- *  Contributor(s): _______________________________________
+ *	Contributor(s): _______________________________________
  *
  *
  ************************************************************************/
@@ -151,8 +151,9 @@ typedef sal_uInt16 rtl_TextEncoding;
 #define RTL_TEXTENCODING_JIS_X_0201 			((rtl_TextEncoding)81)
 #define RTL_TEXTENCODING_JIS_X_0208 			((rtl_TextEncoding)82)
 #define RTL_TEXTENCODING_JIS_X_0212 			((rtl_TextEncoding)83)
+#define RTL_TEXTENCODING_MS_1361				((rtl_TextEncoding)84)
 
-#define RTL_TEXTENCODING_STD_COUNT				((rtl_TextEncoding)84)
+#define RTL_TEXTENCODING_STD_COUNT				((rtl_TextEncoding)85)
 
 #define RTL_TEXTENCODING_USER_START 			((rtl_TextEncoding)0x8000)
 #define RTL_TEXTENCODING_USER_END				((rtl_TextEncoding)0xEFFF)
@@ -216,11 +217,12 @@ Korean (Apple Macintosh)						RTL_TEXTENCODING_APPLE_KOREAN
 Korean (EUC-KR) 								RTL_TEXTENCODING_EUC_KR
 # Korean (ISO-2022-KR)							  RTL_TEXTENCODING_ISO_2022_KR
 Korean (Windows-Wansung-949)					RTL_TEXTENCODING_MS_949
+Korean (Windows-Johab-1361) 					RTL_TEXTENCODING_MS_1361
 
 Latin 3 (ISO-8859-3)							RTL_TEXTENCODING_ISO_8859_3
 
 Japanese (Apple Macintosh)						RTL_TEXTENCODING_APPLE_JAPANESE
-# Japanese (EUC-JP) 							  RTL_TEXTENCODING_EUC_JP
+Japanese (EUC-JP)								RTL_TEXTENCODING_EUC_JP
 # Japanese (ISO-2022-JP)						  RTL_TEXTENCODING_ISO_2022_JP
 Japanese (Shift-JIS)							RTL_TEXTENCODING_SHIFT_JIS
 Japanese (Windows-932)							RTL_TEXTENCODING_MS_932
