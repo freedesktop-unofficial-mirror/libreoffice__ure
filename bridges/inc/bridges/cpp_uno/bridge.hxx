7a6a059ba0e68d55c315b3192cd7012379adae0c
Rüdiger Timm <rt@openoffice.org>
2008-04-11 09:12:00 +0000

    INTEGRATION: CWS changefileheader (1.15.170); FILE MERGED 2008/04/01 15:02:32 thb 1.15.170.3: #i85898# Stripping all external header guards 2008/04/01 10:49:06 thb 1.15.170.2: #i85898# Stripping all external header guards 2008/03/28 16:29:51 rt 1.15.170.1: #i87441# Change license header to LPGL v3.

--- bridges/inc/bridges/cpp_uno/bridge.hxx
+++ bridges/inc/bridges/cpp_uno/bridge.hxx
@@ -1,35 +1,30 @@
 /*************************************************************************
  *
- *  OpenOffice.org - a multi-platform office productivity suite
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ * 
+ * Copyright 2008 by Sun Microsystems, Inc.
  *
- *  $RCSfile: bridge.hxx,v $
+ * OpenOffice.org - a multi-platform office productivity suite
  *
- *  $Revision: 1.15 $
+ * $RCSfile: bridge.hxx,v $
+ * $Revision: 1.16 $
  *
- *  last change: $Author: rt $ $Date: 2005-09-07 22:08:19 $
+ * This file is part of OpenOffice.org.
  *
- *  The Contents of this file are made available subject to
- *  the terms of GNU Lesser General Public License Version 2.1.
+ * OpenOffice.org is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License version 3
+ * only, as published by the Free Software Foundation.
  *
+ * OpenOffice.org is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License version 3 for more details
+ * (a copy is included in the LICENSE file that accompanied this code).
  *
- *    GNU Lesser General Public License Version 2.1
- *    =============================================
- *    Copyright 2005 by Sun Microsystems, Inc.
- *    901 San Antonio Road, Palo Alto, CA 94303, USA
- *
- *    This library is free software; you can redistribute it and/or
- *    modify it under the terms of the GNU Lesser General Public
- *    License version 2.1, as published by the Free Software Foundation.
- *
- *    This library is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- *    Lesser General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public
- *    License along with this library; if not, write to the Free Software
- *    Foundation, Inc., 59 Temple Place, Suite 330, Boston,
- *    MA  02111-1307  USA
+ * You should have received a copy of the GNU Lesser General Public License
+ * version 3 along with OpenOffice.org.  If not, see
+ * <http://www.openoffice.org/license.html>
+ * for a copy of the LGPLv3 License.
  *
  ************************************************************************/
 #ifndef _BRIDGES_CPP_UNO_BRIDGE_HXX_
@@ -38,23 +33,11 @@
 #ifndef _BRIDGES_CPP_UNO_BRIDGE_H_
 #include <bridges/cpp_uno/bridge.h>
 #endif
-
-#ifndef _OSL_MUTEX_HXX_
 #include <osl/mutex.hxx>
-#endif
-#ifndef _RTL_PROCESS_H_
 #include <rtl/process.h>
-#endif
-#ifndef _RTL_USTRBUF_HXX_
 #include <rtl/ustrbuf.hxx>
-#endif
-#ifndef _COM_SUN_STAR_UNO_GENFUNC_HXX_
 #include <com/sun/star/uno/genfunc.hxx>
-#endif
-
-#ifndef _COM_SUN_STAR_UNO_XINTERFACE_HPP_
 #include <com/sun/star/uno/XInterface.hpp>
-#endif
 
 
 namespace CPPU_CURRENT_NAMESPACE
