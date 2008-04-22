e352b806551884f865bea74afae302eecaf5d309
Jens-Heiner Rechtien <hr@openoffice.org>
2008-04-22 13:49:52 +0000

    INTEGRATION: CWS hr50 (1.16.12); FILE MERGED 2008/04/14 15:49:12 hr 1.16.12.1: #i86574#: fix \"control reaches end of non-void function\" warning ( gcc-4.2.3, no optimizations )

--- stoc/source/simpleregistry/simpleregistry.cxx
+++ stoc/source/simpleregistry/simpleregistry.cxx
@@ -7,7 +7,7 @@
  * OpenOffice.org - a multi-platform office productivity suite
  *
  * $RCSfile: simpleregistry.cxx,v $
- * $Revision: 1.17 $
+ * $Revision: 1.18 $
  *
  * This file is part of OpenOffice.org.
  *
@@ -988,13 +988,13 @@ sal_Bool SAL_CALL RegistryKeyImpl::createLink( const OUString& aLinkName, const 
                 throw InvalidRegistryException(
                     OUString( RTL_CONSTASCII_USTRINGPARAM("InvalidRegistryException") ),
                     (OWeakObject *)this );
-            } else
+            } else 
+            {
                 return sal_False;
-        } else
-        {
-            return sal_True;
+            }
         }
-    }		
+    }
+    return sal_True;
 }
 
 //*************************************************************************
