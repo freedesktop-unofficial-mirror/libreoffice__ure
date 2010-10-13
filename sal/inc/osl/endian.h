3d0878b7aa90786eee0a95f68b130a06494d028c
Caolán McNamara <caolanm@redhat.com>
2010-10-13 15:06:44 +0100

    AIX port

--- sal/inc/osl/endian.h
+++ sal/inc/osl/endian.h
@@ -104,11 +104,15 @@ extern "C" {
 #endif
 
 #ifdef AIX
-#	include <sys/machine.h>
+#   include <sys/machine.h>
 #   if BYTE_ORDER == LITTLE_ENDIAN
-#   	define _LITTLE_ENDIAN
+#       ifndef _LITTLE_ENDIAN
+#   	    define _LITTLE_ENDIAN
+#       endif
 #   elif BYTE_ORDER == BIG_ENDIAN
-#   	define _BIG_ENDIAN
+#   	ifndef _BIG_ENDIAN
+#   	    define _BIG_ENDIAN
+#       endif
 #   elif BYTE_ORDER == PDP_ENDIAN
 #   	define _PDP_ENDIAN
 #   endif
